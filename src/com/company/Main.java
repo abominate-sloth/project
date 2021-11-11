6ea683a192f34d745bb751d5d10b151aa66dd50e
abominate-sloth <e.timoshek@mail.ru>
2021-11-11 19:23:40 +0300

    V.1.1.0.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,71 +1,62 @@
 package com.company;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
-
+import java.util.Date;
 
 public class Main {
 
     static int n=0;
-    static String[] a=new String[101];
+    static Date[] a_Date=new Date[101];
+    static String[] a_Task=new String[101];
 
     static void help()
     {
         System.out.println("-help\n-show\n-convertor\n-add\n-delete\n");
     }
 
-    static void add(String s)
+    static int add(Date day,String s)
     {
-        int i;
-        String g;
-        g="";
-
-        for (i=5;i<s.length();i++)
-            g=g+s.charAt(i);
-
-        a[n]=g;
-        n=n+1;
+        return 1;
     }
 
-    static void show()
+    static int show(Date day)
     {
-        int i=0;
-        for(i=0;i<n;i++)
-            System.out.println(a[i]);
+        return 1;
     }
 
-    static void delete(String s)
+    static int delete(Date day)
     {
-        int i;
-        String g;
-        a[n]="";
-        g="";
-
-        for (i=8;i<s.length();i++)
-            g=g+s.charAt(i);
-
-        i=0;
-
-        while(!(a[i].equals(g))&&(i<n))
-            i++;
-
-        if(i<n)
-        {n--;a[i]=a[n];}
-        else System.out.println("Такой записи не найдено");
-
+        return 1;
     }
 
-    static void convert(String s)
+    static int convert(String s)
     {
-        System.out.println("В разработке");
+        return 1;
     }
 
     public static void main(String[] args) {
 
-            int p=0,i;
+        Scanner in = new Scanner(System.in);
+            int p=0,i,chek;
+            long x;
+            Date day= new Date();
             String s,v;
+            /*strDate="12.10.2020";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        GregorianCalendar calendar = new GregorianCalendar();
 
-            Scanner in = new Scanner(System.in);
+        Date day= ;new Date();
 
-            while(p==0)
+        try {
+            day = formatter.parse(strDate);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }*/
+
+        while(p==0)
             {
                 s=in.nextLine();
                 s=s+" ";
@@ -75,23 +66,36 @@ public class Main {
                 while(s.charAt(++i)!=' ')
                     v=v+s.charAt(i);
 
-                if(v.equals("-add"))
-                    add(s);
+                switch (v) {
+                    case "добавить":
+                        chek=add(day,s);
+                        if(chek==2)
+                            System.out.println("Такая запись уже есть");
+                        break;
+                    case "просмотреть":
+                        chek=show(day);
+                        if(chek==2)
+                            System.out.println("Ничего не найдено");
+                        break;
+                    case "помощь": help();
+                        break;
+                    case "удалить": chek=delete(day);
+                        if(chek==2)
+                            System.out.println("Таких записей не найдено");
 
-                if(v.equals("-show"))
-                    show();
+                        break;
+                    case "конвертировать":
+                        chek=convert(s);
+                        if(chek==-1)
+                            System.out.println("Формат не совпадает");
+                        break;
+                    case "выход": p=1;
+                        break;
 
-                if(v.equals("-help"))
-                    help();
+                    default:System.out.println("Команда не распознана");
+                        break;
+                }
 
-                if(v.equals("-delete"))
-                    delete(s);
-
-                if(v.equals("-convert"))
-                    convert(s);
-
-                if(v.equals("-exit"))
-                    p=1;
 
             }
 
